5ba7062e0d936fab8c44c5ffa4a8e6f3cde5b808
tsvetantodorov <tstodorofff@gmail.com>
2022-11-14 18:31:11 +0200

    Adding EvenNumber

---
+++ src/BasicSyntaxConditionalStatementsAndLoops/Lab/EvenNumber.java
@@ -0,0 +1,23 @@
+package BasicSyntaxConditionalStatementsAndLoops.Lab;
+
+import java.util.Scanner;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        while (num % 2 != 0) {
+            System.out.println("Please write an even number.");
+            num = Integer.parseInt(scanner.nextLine());
+        }
+
+        if (num < 0) {
+            System.out.printf("The number is: %d", Math.abs(num));
+        } else {
+            System.out.printf("The number is: %d", num);
+        }
+    }
+}
