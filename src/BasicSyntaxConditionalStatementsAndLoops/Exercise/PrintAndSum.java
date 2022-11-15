1b33363e17a1621da3b383bd3507819016a9504b
tsvetantodorov <tstodorofff@gmail.com>
2022-11-15 15:31:13 +0200

    Adding PrintAndSum

---
+++ src/BasicSyntaxConditionalStatementsAndLoops/Exercise/PrintAndSum.java
@@ -0,0 +1,21 @@
+package BasicSyntaxConditionalStatementsAndLoops.Exercise;
+
+import java.util.Scanner;
+
+public class PrintAndSum {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int startNum = Integer.parseInt(scanner.nextLine());
+        int endNum = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        for (int i = startNum; i <= endNum; i++) {
+            sum += i;
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
+        System.out.printf("Sum: %d", sum);
+    }
+}
