a709a98a037b31aab316de227d716cdeaf7e5e27
tsvetantodorov <tstodorofff@gmail.com>
2022-11-18 15:52:08 +0200

    Adding ConvertMetersInKilometers

---
+++ src/BasicSyntaxConditionalStatementsAndLoops/Lab/ConvertMetersToKilometers.java
@@ -0,0 +1,16 @@
+package BasicSyntaxConditionalStatementsAndLoops.Lab;
+
+import java.util.Scanner;
+
+public class ConvertMetersToKilometers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int distanceInMeters = Integer.parseInt(scanner.nextLine());
+
+        double distanceInKilometers = distanceInMeters * 1.00 / 1000;
+
+        System.out.printf("%.2f", distanceInKilometers);
+    }
+}
