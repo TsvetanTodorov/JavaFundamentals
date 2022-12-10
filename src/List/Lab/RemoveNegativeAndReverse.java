efbf881f53665442d97b6dc81a2b2a344f0969c7
tsvetantodorov <tstodorofff@gmail.com>
2022-12-10 16:20:56 +0200

    Adding RemoveNegativeAndReverse

---
+++ src/List/Lab/RemoveNegativeAndReverse.java
@@ -0,0 +1,30 @@
+package List.Lab;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class RemoveNegativeAndReverse {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        numbers.removeIf(n -> n < 0);
+        Collections.reverse(numbers);
+        System.out.println();
+
+        if (numbers.isEmpty()) {
+            System.out.println("empty");
+        } else {
+            System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
+        }
+    }
+
+}
