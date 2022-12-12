fd664dfbb91aa74084bb683b14e185fe86f07da1
tsvetantodorov <tstodorofff@gmail.com>
2022-12-12 14:17:04 +0200

    Refactoring Students

--- src/ObjectsAndClasses/Lab/Students.java
+++ src/ObjectsAndClasses/Lab/Students.java
@@ -14,20 +14,12 @@ public class Students {
 
         while (!input.equals("end")) {
             String[] inputStudent = input.split(" ");
-            Student student = new Student(inputStudent[0], inputStudent[1], Integer.parseInt(inputStudent[2]), inputStudent[3]);
+            Student student = new Student(inputStudent[0], inputStudent[1],
+                    Integer.parseInt(inputStudent[2]), inputStudent[3]);
             studentsList.add(student);
             input = scanner.nextLine();
         }
-
         String targetCity = scanner.nextLine();
-
-//        for (int i = 0; i <= studentsList.size() - 1; i++) {
-//            Student currentStudent = studentsList.get(i);
-//            if (currentStudent.hometown.equals(targetCity)) {
-//                System.out.printf("%s %s is %d years old%n", currentStudent.firstName, currentStudent.lastName,
-//                        currentStudent.age);
-//            }
-//        }
 
         for (Student currentStudent : studentsList) { //
             if (currentStudent.hometown.equals(targetCity)) {
@@ -35,20 +27,7 @@ public class Students {
                         currentStudent.age);
             }
         }
-
-//        List<Integer> myNumbers = new ArrayList<>();
-//        myNumbers.add(10);
-//        myNumbers.add(5);
-//        myNumbers.add(2);
-//        myNumbers.add(1);
-//
-//        for (Integer currentNumber : myNumbers) {
-//            System.out.println(currentNumber);
-//        }
-
-
     }
-
     public static class Student {
         public String firstName;
         public String lastName;
