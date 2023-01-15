95f26c983291b433fd07757345d5f86c623ce237
tsvetantodorov <tstodorofff@gmail.com>
2023-01-15 17:34:47 +0200

    Adding AppendArrays

---
+++ src/List/Exercise/AppendArrays.java
@@ -0,0 +1,19 @@
+package List.Exercise;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AppendArrays {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] arrays = scanner.nextLine().split("\\|");
+        List<String[]> listOfArrays = Arrays.stream(arrays).map(array -> array.split("\\s+")).collect(Collectors.toList());
+        Collections.reverse(listOfArrays);
+        listOfArrays.forEach(a -> {
+            Arrays.stream(a).forEach(el -> System.out.print(!el.equals("") ? el + " " : ""));
+        });
+    }
+
+}
