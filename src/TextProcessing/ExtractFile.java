9fe01b624345d8220d674faa576f37da9e6856b0
tsvetantodorov <tstodorofff@gmail.com>
2023-02-03 16:59:28 +0200

    Adding ExtractFile

---
+++ src/TextProcessing/ExtractFile.java
@@ -0,0 +1,22 @@
+package TextProcessing;
+
+import java.util.Scanner;
+
+public class ExtractFile {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String fullPathFile = scanner.nextLine();
+        String[] pathParts = fullPathFile.split("\\\\");
+        String fullFileName = pathParts[pathParts.length - 1];
+        String fileName = fullFileName.split("\\.")[0];
+        String extension = fullFileName.split("\\.")[1];
+
+
+        System.out.printf("File name: %s%n", fileName);
+        System.out.printf("File extension: %s", extension);
+
+
+    }
+}
